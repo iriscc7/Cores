9ad8ad759129bfb5b459d1f0d6a8b14242be05c5
iriscc7 <raffaella240906@gmail.com>
2025-11-01 20:46:29 -0500

    .

---
+++ PrimerProyectodeReactyVite_IC/Primer_Proyecto_ReacyVite_IC/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <header></header>
+    <main>
+      <h1>¡Bienvenido a mi aplicación de React!</h1>
+      <h2>Lista de cosas por hacer:</h2>
+      <ol>
+        <li>Aprender React</li>
+        <li>Practicar con Vite</li>
+        <li>Construir proyectos increíbles</li>
+      </ol>
+      </main>
+    <footer></footer>
+
+    </>
+  )
+}
+
+export default App

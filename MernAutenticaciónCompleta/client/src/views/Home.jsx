514d01ad4863e166be408313c4b5f4f6db7e1368
iriscc7 <raffaella240906@gmail.com>
2025-12-08 12:11:54 -0500

    Código para ambas cores (server y client)

--- MernAutenticaciónCompleta/client/src/views/Home.jsx
+++ MernAutenticaciónCompleta/client/src/views/Home.jsx
@@ -3,7 +3,7 @@
 const Home=()=>{
 return (
     <>
-        <h1>Bienvenido a Al Ritmo de Mern con Mern Autenticación Server</h1>
+        <h1>Bienvenido a Al Ritmo de Mern conMern Autenticación Completa</h1>
         <p>Aquí podrás crear,editar y eliminar todas las canciones y playlist que quieras,pero ahora con Login y Registro</p>
         <img src="https://pa1.aminoapps.com/6639/f0ae73011955b79f2c86f560dce87bd3f9c456e5_hq.gif" alt="Pingu con audífonos"/>
     </>
